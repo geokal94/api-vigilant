8da8a6f630d4ce220ef9399bdcae1c8a91428784
Giorgos Kallis <giorgos.kallis.gr@gmail.com>
2024-01-05 17:58:10 +0200

    feat: add views

--- api_monitor/views.py
+++ api_monitor/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Endpoint, TestResult
+from .serializers import EndpointSerializer, TestResultSerializer
+
+
+class EndpointListCreateView(generics.ListCreateAPIView):
+    queryset = Endpoint.objects.all()
+    serializer_class = EndpointSerializer
+
+class EndpointDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Endpoint.objects.all()
+    serializer_class = EndpointSerializer
+
+class TestResultListView(generics.ListAPIView):
+    serializer_class = TestResultSerializer
+
+    def get_queryset(self):
+        endpoint_id = self.kwargs['endpoint_id']
+        return TestResult.objects.filter(endpoint_id=endpoint_id)
