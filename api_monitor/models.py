8bde603c88c42ea27079deca97f3925c6c801e51
Giorgos Kallis <giorgos.kallis.gr@gmail.com>
2024-01-05 17:57:55 +0200

    feat: add models

--- api_monitor/models.py
+++ api_monitor/models.py
@@ -4,23 +4,18 @@ from django.db import models
 class Endpoint(models.Model):
     name = models.CharField(max_length=100)
     url = models.URLField()
-    method = models.CharField(max_length=10)
-    headers = models.TextField()
-    expected_status = models.IntegerField()
+    method = models.CharField(max_length=10, choices=[('GET', 'GET'), ('POST', 'POST'), ('PUT', 'PUT'), ('DELETE', 'DELETE')])
+    headers = models.JSONField(default=dict)
     check_interval = models.DurationField()
 
     def __str__(self):
         return self.name
 
 class TestResult(models.Model):
-    endpoint = models.ForeignKey(Endpoint, on_delete=models.CASCADE)
+    endpoint = models.ForeignKey(Endpoint, related_name='test_results', on_delete=models.CASCADE)
     status_code = models.IntegerField()
-    response_time = models.DurationField()
-    response_body = models.TextField()
-    passed_validation = models.BooleanField()
+    response_time = models.FloatField()  # Store response time in seconds
     timestamp = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-timestamp']
-
-# Add more models as needed for TestConfiguration, Alert, etc.
